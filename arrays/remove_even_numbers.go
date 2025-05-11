99ad8f0909ffb0fb2612ab4b5f0a305db7e72bc5
luiznwz <luizrenan179@gmail.com>
2025-05-10 21:05:47 -0300

    Implement function to remove even numbers from an array and add main function for testing

---
+++ arrays/remove_even_numbers.go
@@ -0,0 +1,16 @@
+package main
+w
+import "fmt"
+
+func main() {
+	fmt.Println(removeEvenNumbersFromArray([]int{1, 2, 3, 4, 5}))
+}
+
+func removeEvenNumbersFromArray(arr []int) (oddNumbers []int) {
+	for i := 0; i < len(arr); i++ {
+		if arr[i]%2 != 0 {
+			oddNumbers = append(oddNumbers, arr[i])
+		}
+	}
+	return
+}
